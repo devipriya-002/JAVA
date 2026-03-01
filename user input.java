d001f4deb00cfd2eaba4fd3ff24bfd1fd02e4d2a
devipriya-002 <priya.123dkv@gmail.com>
2026-03-01 11:37:43 +0530

    Create user input.java

---
+++ user input.java
@@ -0,0 +1,42 @@
+
+##String
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc(varaible) = new Scanner(System.in);
+        String str =sc.nextLine();
+        System.out.println("Try programiz.pro");
+    }
+}
+
+##Character
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    char ch = sc.nextLine().charAt(0);
+		System.out.println(ch);
+	}
+}
+
+##Byte,Int,Long,Short,Double,Float
+ import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    char ch = sc.nextLine().charAt(3);
+	    byte a = sc.nextByte();
+	    int b = sc.nextInt();
+	    long c = sc.nextLong();
+	    short d = sc.nextShort();
+	    double e = sc.nextDouble();
+	    float f = sc.nextFloat();
+		System.out.println(ch+" "+a);
+		System.out.println(b+" "+c);
+	    System.out.println(d+" "+e);
+	     System.out.println(f);
+		
+	}
+}
