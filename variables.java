ba540c581a54b9d5d67aea31520fbe7e2eb8c830
devipriya-002 <priya.123dkv@gmail.com>
2026-07-05 15:24:30 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,22 @@
+int myNum = 5;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool = true;
+String myText = "Hello";
+
+//print
+String firstName = "John ";
+String lastName = "Doe";
+String fullName = firstName + lastName;
+System.out.println(fullName);
+
+int x = 5;
+int y = 6;
+
+System.out.println("The sum is " + x + y);   
+System.out.println("The sum is " + (x + y)); 
+
+//multiple variables
+int x, y, z;
+x = y = z = 50;
+System.out.println(x + y + z);
