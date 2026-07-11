237cfc0c69b7d233de996470992bd3c7a85a970a
devipriya-002 <priya.123dkv@gmail.com>
2026-07-11 10:34:48 +0530

    Java string operations

---
+++ string.java
@@ -0,0 +1,14 @@
+//Java Strings
+String txt = "Hello World";
+System.out.println(txt.toUpperCase());  
+System.out.println(txt.toLowerCase());  
+
+String firstName = "John";
+String lastName = "Doe";
+System.out.println(firstName + " " + lastName);
+
+String x = "10";
+String y = "20";
+String z = x + y;
+
+String txt = "It\'s alright.";
