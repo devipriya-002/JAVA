e6323705d72e7ee3f5107f4d1844b19f3164f0df
devipriya-002 <priya.123dkv@gmail.com>
2026-07-11 10:28:51 +0530

    java data types
    Added boolean, char, and var examples to datatypes.java.

--- datatypes.java
+++ datatypes.java
@@ -15,3 +15,19 @@ System.out.println(myNum);
 
 double myNum = 19.99d;
 System.out.println(myNum);
+
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println(isJavaFun);    
+System.out.println(isFishTasty); 
+
+char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+System.out.println(myVar1);
+System.out.println(myVar2);
+System.out.println(myVar3);
+
+var myNum = 5;         // int
+var myDouble = 9.98;   // double
+var myChar = 'D';      // char
+var myBoolean = true;  // boolean
+var myString = "Hello"; // String
